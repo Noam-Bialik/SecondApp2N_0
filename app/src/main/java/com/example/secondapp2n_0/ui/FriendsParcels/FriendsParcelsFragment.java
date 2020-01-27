09cb8793defd18c9b2f614436b16e67998767ee7
navesarussi <navesarussi@gmail.com>
2020-01-27 03:52:54 +0200

    סתם

--- app/src/main/java/com/example/secondapp2n_0/ui/FriendsParcels/FriendsParcelsFragment.java
+++ app/src/main/java/com/example/secondapp2n_0/ui/FriendsParcels/FriendsParcelsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,21 +12,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.example.secondapp2n_0.Data.IParcelsDateSource;
-import com.example.secondapp2n_0.Data.IParcelsRepository;
-import com.example.secondapp2n_0.Data.ParcelDataSource;
-import com.example.secondapp2n_0.Data.ParcelsRepository;
 import com.example.secondapp2n_0.R;
+
 
 public class FriendsParcelsFragment extends Fragment {
 
     private FriendsParcelsViewModel galleryViewModel;
-    private IParcelsRepository parcelsRepository;
 
-
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        parcelsRepository= ParcelsRepository.getInstance();
-        galleryViewModel = ViewModelProviders.of(this).get(FriendsParcelsViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        galleryViewModel =
+                ViewModelProviders.of(this).get(FriendsParcelsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
         galleryViewModel.getText().observe(this, new Observer<String>() {
@@ -36,17 +31,6 @@ public class FriendsParcelsFragment extends Fragment {
                 textView.setText(s);
             }
         });
-        Button startPDS = root.findViewById(R.id.startParcelsDataSource);
-        startPDS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                IParcelsDateSource a = ParcelDataSource.getInstance();
-            }
-        });
         return root;
-    }
-    public void Delivery(View view)
-    {
-
     }
 }
